9e2ab1e9b81611f387c668368f6235c207dfb76b
Elias <esantospinto@gmail.com>
2025-01-06 11:41:32 -0300

    Criado o cadastro de usuario

---
+++ SGE.Web.UI/Controllers/CadastroUsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGE.Aplicacao.DTOs;
+using SGE.Aplicacao.Interfaces;
+
+namespace SGE.Web.UI.Controllers
+{
+    public class CadastroUsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public CadastroUsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(UsuarioDTO usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuarioValid = await _usuarioService.BuscarUsuarioAsync(usuario.UsuarioLogin);
+
+                if (string.IsNullOrEmpty(usuarioValid.UsuarioLogin))
+                {
+                    var usuarioCadastrado = await _usuarioService.CriarUsuarioAsync(usuario);
+
+                    if (usuarioCadastrado.UsuarioLogin != null)
+                    {
+                        // Redireciona para a página de login ou home após o cadastro
+                        return RedirectToAction("Index", "Login");
+                    }
+
+                }
+                else {
+
+                    ModelState.AddModelError(string.Empty, "Usuário já cadastrado.");
+                    return View("Index");
+                }
+
+                ModelState.AddModelError(string.Empty, "Erro ao cadastrar usuário.");
+            }
+
+            return RedirectToAction("Index","Login");
+        }
+    }
+}
